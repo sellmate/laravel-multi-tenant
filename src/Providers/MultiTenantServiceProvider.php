28032aaf8f9c3db40695a99b0e03bb7ce9ac03d3
Eunjun Park <ej@sellmate.co.kr>
2021-04-08 18:11:39 +0900

    stubs publish 경로 오류 수정

--- src/Providers/MultiTenantServiceProvider.php
+++ src/Providers/MultiTenantServiceProvider.php
@@ -30,9 +30,9 @@ class MultiTenantServiceProvider extends ServiceProvider
             __DIR__.'/../../config/multitenancy.php' => config_path('multitenancy.php'),
             __DIR__.'/../../database/migrations/' => database_path('migrations'),
             __DIR__.'/../../database/Tenant.php' => app_path('Models/System/Tenant.php'),
-            __DIR__.'/../Commands/stubs/model.stub' => app_path('stubs/model.stub'),
-            __DIR__.'/../Commands/stubs/model.pivot.stub' => app_path('stubs/model.pivot.stub'),
-            __DIR__.'/../Commands/Seeds/stubs/seeder.stub' => app_path('stubs/seeder.stub'),
+            __DIR__.'/../Commands/stubs/model.stub' => base_path('stubs/model.stub'),
+            __DIR__.'/../Commands/stubs/model.pivot.stub' => base_path('stubs/model.pivot.stub'),
+            __DIR__.'/../Commands/Seeds/stubs/seeder.stub' => base_path('stubs/seeder.stub'),
         ]);
     }
 
