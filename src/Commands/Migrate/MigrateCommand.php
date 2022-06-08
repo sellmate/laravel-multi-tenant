2863403d24f3532519f30d373e8234106e8fac28
Eunjun Park <ej@sellmate.co.kr>
2022-06-08 17:21:08 +0900

    SqlServer 스키마 로드 가능하도록 migrate 명령어 개선

--- src/Commands/Migrate/MigrateCommand.php
+++ src/Commands/Migrate/MigrateCommand.php
@@ -4,6 +4,7 @@ namespace Sellmate\Laravel\MultiTenant\Commands\Migrate;
 
 use Illuminate\Contracts\Events\Dispatcher;
 use Illuminate\Database\Console\Migrations\MigrateCommand as BaseMigrateCommand;
+use Illuminate\Database\Events\SchemaLoaded;
 use Sellmate\Laravel\MultiTenant\Commands\EnvCheck;
 use Sellmate\Laravel\MultiTenant\Commands\TenantCommand;
 use Sellmate\Laravel\MultiTenant\DatabaseManager;
@@ -24,6 +25,7 @@ class MigrateCommand extends BaseMigrateCommand
                 {--force : Force the operation to run when in production}
                 {--path=* : The path(s) to the migrations files to be executed}
                 {--realpath : Indicate any provided migration file paths are pre-resolved absolute paths}
+                {--schema-path= : The path to a schema dump file}
                 {--pretend : Dump the SQL queries that would be run}
                 {--seed : Indicates if the seed task should be re-run}
                 {--step : Force the migrations to be run so they can be rolled back individually}";
@@ -67,5 +69,60 @@ class MigrateCommand extends BaseMigrateCommand
             $this->setSystemDatabase();
             parent::handle();
         }
+    }
+
+    /**
+     * Prepare the migration database for running.
+     *
+     * @return void
+     */
+    protected function prepareDatabase()
+    {
+        // INFO: 스키마 상태 불러오기 순서를 변경해서 migrations 테이블을 불필요하게 만들었다 지우는 과정 생략
+        if (! $this->migrator->hasRunAnyMigrations() && ! $this->option('pretend')) {
+            $this->loadSchemaState();
+        }
+
+        if (! $this->migrator->repositoryExists()) {
+            $this->call('migrate:install', array_filter([
+                '--database' => $this->option('database'),
+            ]));
+        }
+    }
+
+    /**
+     * Load the schema state to seed the initial database schema structure.
+     *
+     * @return void
+     */
+    protected function loadSchemaState()
+    {
+        $connection = $this->migrator->resolveConnection($this->option('database'));
+
+        // First, we will make sure that the connection supports schema loading and that
+        // the schema file exists before we proceed any further. If not, we will just
+        // continue with the standard migration operation as normal without errors.
+        if (! is_file($path = $this->schemaPath($connection))) {
+            return;
+        }
+
+        $this->line('<info>Loading stored database schema:</info> '.$path);
+
+        $startTime = microtime(true);
+
+        $connection->getSchemaState()->handleOutputUsing(function ($type, $buffer) {
+            $this->output->write($buffer);
+        })->load($path);
+
+        $runTime = number_format((microtime(true) - $startTime) * 1000, 2);
+
+        // Finally, we will fire an event that this schema has been loaded so developers
+        // can perform any post schema load tasks that are necessary in listeners for
+        // this event, which may seed the database tables with some necessary data.
+        $this->dispatcher->dispatch(
+            new SchemaLoaded($connection, $path)
+        );
+
+        $this->line('<info>Loaded stored database schema.</info> ('.$runTime.'ms)');
     }
 }
